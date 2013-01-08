18577073f7bca2f0973d389e6f9d7628cf8c1294
Jobfully Admin <admin@jobfully.com>
2013-01-07 23:46:38 -0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtest::Application.config.secret_token = '6b9780b08ac9d52313a9683374b6e2ffc4d2a0ef5c870b10eca1687b27a53e4670fec24168e1c2409f0a73f9054d3d7908eaa05e2fb685c4d955ea64ebaed9fb'

7c4e53f0d68bc67a7918ff2edd5dfa4bafd21af5
Jobfully Admin <admin@jobfully.com>
2013-01-19 20:53:15 -0800

    initial setup: 1. created pages controller, 2. added landing page html, 3. added modernizr-latest.js, 4. added normalize.css, 5. added _header partial

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def landing
+  end
+
+  def program
+  end
+
+  def agency
+  end
+
+  def companyhr
+  end
+
+  def custom
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Gtest::Application.routes.draw do
+  get "pages/landing"
+
+  get "pages/program"
+
+  get "pages/agency"
+
+  get "pages/companyhr"
+
+  get "pages/custom"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,7 @@ Gtest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#landing'
 
   # See how all your routes lay out with "rake routes"
 
